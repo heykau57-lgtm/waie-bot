4fc30c6af7707fc020c031952c0df7f73b343e76
heykau57-lgtm <heykau57@gmail.com>
2026-04-05 15:54:44 +0800

    Add configuration for bot settings

--- database/config.js
+++ database/config.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    tokens: "8212739649:AAH4x6vCnYg_mj4fBRKar4Sl-ZcT1NzjzmM",
+    owners: ["8429302432 "],
+    ipvps: "waie-bot.vercel.app",
+    port: 3000,
+    botName: "Waie Bot"
+}; 
